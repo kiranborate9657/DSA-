8f6350d6f97dde7a748b173486e5ba9f4af41ed5
Kiran Borate <boratek844@gmail.com>
2025-11-21 11:37:03 +0530

    Take name as input and print a greeting message for that particular name.

---
+++ greeting.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+public class greeting {
+    public static void main(String[] args) {
+        String a;
+        
+        System.out.println("Enter the name::");
+        Scanner sc = new Scanner(System.in);
+        a=sc.nextLine();
+        System.out.println("Hello  " +  a  + "  Welcome");
+        
+
+    }
+}
