df8d7421e62ff11a4054f33d5b74871dcf487aa7
Kiran Borate <boratek844@gmail.com>
2026-01-01 10:55:27 +0530

    solid square pattern

---
+++ pattern1.java
@@ -0,0 +1,20 @@
+
+public class pattern1 {
+
+    public static void main(String[] arg)
+    {
+        System.out.println("Printing the solid square pattern");
+
+        for(int i=1;i<=4;i++){
+
+            for(int j=1;j<=4;j++)
+            {
+             System.out.print("*");
+            }
+
+          System.out.println( );
+        }
+    
+    }
+    
+}
