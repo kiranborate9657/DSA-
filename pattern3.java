e1306bf1e6854954c042394b721ed5e96a697a42
Kiran Borate <boratek844@gmail.com>
2026-01-01 16:09:31 +0530

    left half pyramid

---
+++ pattern3.java
@@ -0,0 +1,23 @@
+public class pattern3 {
+    
+    public static void main(String [] args)
+    {
+        System.out.println("Printing the pattern");
+        for (int i=1;i<=5;i++)
+        {
+
+            for(int k=5;k>i;k--)
+            {
+               System.out.print(" ");
+            }
+
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+        
+    }
+}
