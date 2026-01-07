27d4632c81ed9b97b5da6997ecd28a157999d489
Kiran Borate <boratek844@gmail.com>
2026-01-07 22:38:21 +0530

    printing the inverted traingle

--- Revise/trianglepattern.java
+++ Revise/trianglepattern.java
@@ -2,14 +2,16 @@
 
 public class trianglepattern {
     public static void main(String[] args) {
-        for(int i=1;i<=5;i++)
-        {
-            for(int s=1;s<=5-i;s++)
-            {
-               System.out.print(" ");
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" *");
             }
-            for(int j=1;j<=i;j++)
-            {
+            System.out.println();
+        }
+
+        // Second half
+        for (int i = 4; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print(" *");
             }
             System.out.println();
