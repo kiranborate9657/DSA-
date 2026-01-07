4e75eefce75ca8160972f918c20143d2fdb62bf1
Kiran Borate <boratek844@gmail.com>
2026-01-07 10:52:06 +0530

    call the method with the return value

---
+++ stringfunction.java
@@ -0,0 +1,12 @@
+public class stringfunction {
+    public static void main(String[] args) {
+         String message =name();
+         System.out.println(message);
+        
+    }
+    static String name()
+    {
+       String names="Kiran Borate";
+       return names;
+    }
+}
