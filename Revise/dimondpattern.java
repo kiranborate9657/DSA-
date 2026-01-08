8b6f9604145c6856c18d9fdd0712a794198f0e88
Kiran Borate <boratek844@gmail.com>
2026-01-08 20:05:57 +0530

    printing the dimond  pattern

---
+++ Revise/dimondpattern.java
@@ -0,0 +1,29 @@
+public class dimondpattern {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++)
+        {
+            for(int s=1;s<=5-i;s++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int i=1;i<=4;i++)
+        {
+            for(int s=1;s<=i;s++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=5-i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
