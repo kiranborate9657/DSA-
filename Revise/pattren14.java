f52b0d32c243ca25a7ad662a1e026e18ad163506
Kiran Borate <boratek844@gmail.com>
2026-01-10 21:44:21 +0530

    printing the numeric pattern

---
+++ Revise/pattren14.java
@@ -0,0 +1,20 @@
+public class pattren14 {
+    public static void main(String[] args) {
+        for(int i=1;i<=7;i++)
+        {
+            for(int j=1;j<9-i;j++)
+            {
+                System.out.print(j+"");
+            }
+            System.out.println();
+        }
+        for(int i=1;i<=6;i++)
+        {
+            for(int j=1;j<=1+i;j++)
+            {
+                System.out.print(j+"");
+            }
+            System.out.println();
+        }
+    }
+}
