f076b2bfdf4eba253e88a93debcbab38b2114b13
Kiran Borate <boratek844@gmail.com>
2026-01-10 18:19:29 +0530

    printing the numeric pattern

---
+++ Revise/pattern12.java
@@ -0,0 +1,18 @@
+public class pattern12 {
+    public static void main(String[] args) {
+        for(int i=1;i<=7;i++)
+        {
+            for(int s=1;s<=7-i;s++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++)
+            {
+                //System.out.print(String.valueOf(j));
+                System.out.print( j );
+            }
+            System.out.println();
+        }
+    }
+    }
+
