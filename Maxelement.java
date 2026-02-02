99ce6ef1e657de2968c279427b3d8e2fb29339b5
Kiran Borate <boratek844@gmail.com>
2026-02-02 23:07:55 +0530

    Create Maxelement.java

---
+++ Maxelement.java
@@ -0,0 +1,13 @@
+class LargestElement {
+    public static void main(String[] args) {
+        int[] arr = {45, 12, 78, 34, 89};
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max)
+                max = arr[i];
+        }
+
+        System.out.println("Largest element: " + max);
+    }
+}
