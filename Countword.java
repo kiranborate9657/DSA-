61b7fb7ec02958a7eeda744eebf6f3b19d802c65
Kiran Borate <boratek844@gmail.com>
2026-02-04 19:50:09 +0530

    Create Countword.java

---
+++ Countword.java
@@ -0,0 +1,7 @@
+class CountWords {
+    public static void main(String[] args) {
+        String s = "Java is easy";
+        String[] words = s.split(" ");
+        System.out.println("Word count: " + words.length);
+    }
+}
